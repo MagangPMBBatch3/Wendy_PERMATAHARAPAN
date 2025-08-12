0598e1c2fb9e45248349d3e088556f5979c89363
unknown <wendywendi2019@gmail.com>
2025-08-12 16:19:54 +0700

    Schema Bagian

---
+++ app/Models/Bagian/Bagians.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\Bagian;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bagians extends Model
+{
+    protected $table = 'bagian';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = ['nama'];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public $timestamps = false;
+}
--- database/migrations/2025_08_12_042036_bagian/create_bagian_table.php
+++ database/migrations/2025_08_12_042036_bagian/create_bagian_table.php
@@ -11,7 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('bagian', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama',100);
+            $$table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('bagian');
     }
 };
